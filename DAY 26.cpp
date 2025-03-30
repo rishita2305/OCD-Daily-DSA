44f0c85f3f4ac10e43167d9751637dad4df4af89
rishita2305 <rishita.chaudhary2020@gmail.com>
2025-03-30 19:58:47 +0530

    Update DAY 26.cpp

--- DAY 26.cpp
+++ DAY 26.cpp
@@ -25,5 +25,31 @@ class Solution {
 };
 
 //Question 1) Doubly linked list Insertion at given position
-//Approach:
-// TC: O()  SC: O() 
+//Approach: traverse through the array to find the position where element is to be inserted.
+// TC: O(n)  SC: O(1) 
+class Solution {
+  public:
+    Node *addNode(Node *head, int pos, int data) {
+        if (head == NULL) return head;
+        
+        Node* newNode = new Node(data);
+        Node* temp = head;
+        
+        // Traverse to the position where the new node should be inserted
+        for (int i = 0; i < pos && temp->next != NULL; i++) {
+            temp = temp->next;
+        }
+        
+        // Insert the new node after temp
+        newNode->next = temp->next;
+        newNode->prev = temp;
+        temp->next = newNode;
+        
+        // If the new node is not the last node, update the next node's previous pointer
+        if (newNode->next != NULL) {
+            newNode->next->prev = newNode;
+        }
+        
+        return head;
+    }
+};
